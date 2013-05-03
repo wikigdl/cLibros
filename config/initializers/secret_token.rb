899aeb1aa9debb4e766f632b2836e12aea2af6dd
sergio molina <wikigdl@gmail.com>
2013-05-02 21:18:34 -0500

    firs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CLibros::Application.config.secret_token = '009b693ea1459e48449e67e059dee26dd6dbe7019a13a67743c2ee3c52e6b78d0a54f84c17f233cf36c345101ef5019752b7a63db4604d2a88bfa1eed3966c85'
